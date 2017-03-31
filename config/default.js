77a6d58a843821cf0d6b425feff4bde4036168af
sukitha <sukitha@duosoftware.com>
2017-03-31 11:09:52 +0530

    service url changed in default.js

--- config/default.js
+++ config/default.js
@@ -53,7 +53,7 @@ module.exports = {
 
   "Services": {
     "accessToken": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdWtpdGhhIiwianRpIjoiMTdmZTE4M2QtM2QyNC00NjQwLTg1NTgtNWFkNGQ5YzVlMzE1Iiwic3ViIjoiNTZhOWU3NTlmYjA3MTkwN2EwMDAwMDAxMjVkOWU4MGI1YzdjNGY5ODQ2NmY5MjExNzk2ZWJmNDMiLCJleHAiOjE4OTMzMDI3NTMsInRlbmFudCI6LTEsImNvbXBhbnkiOi0xLCJzY29wZSI6W3sicmVzb3VyY2UiOiJhbGwiLCJhY3Rpb25zIjoiYWxsIn1dLCJpYXQiOjE0NjEyOTkxNTN9.YiocvxO_cVDzH5r67-ulcDdBkjjJJDir2AeSe3jGYeA",
-    "callcontrolHost": "resourceservice.104.131.67.21.xip.io",
+    "callcontrolHost": "resourceservice.app.veery.cloud",
     "callcontrolPort": "8831",
     "callcontrolVersion": "1.0.0.0",
 
