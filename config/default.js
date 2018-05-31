a85f32f8fdaee129270def86cb1033f183d3b3b6
sine19 <sineth17@gmail.com>
2018-05-31 18:43:15 +0530

    clear all values
    all default.js values are cleared due to security reasons.

--- config/default.js
+++ config/default.js
@@ -1,23 +1,23 @@
 module.exports = {
   "DB": {
     "Type": "postgres",
-    "User": "duo",
-    "Password": "DuoS123",
+    "User": "",
+    "Password": "",
     "Port": 5432,
     "Host": "localhost",
-    "Database": "dvpdb"
+    "Database": ""
   },
 
 
   "Redis":
   {
     "mode":"sentinel",//instance, cluster, sentinel
-    "ip": "45.55.142.207",
+    "ip": "",
     "port": 6389,
-    "user": "duo",
-    "password": "DuoS123",
+    "user": "",
+    "password": "",
     "sentinels":{
-      "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
+      "hosts": "",
       "port":16389,
       "name":"redis-cluster"
     }
@@ -28,13 +28,13 @@ module.exports = {
   "Security":
   {
 
-    "ip" : "45.55.142.207",
+    "ip" : "",
     "port": 6389,
-    "user": "duo",
-    "password": "DuoS123",
+    "user": "",
+    "password": "",
     "mode":"sentinel",//instance, cluster, sentinel
     "sentinels":{
-      "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
+      "hosts": "",
       "port":16389,
       "name":"redis-cluster"
     }
@@ -59,20 +59,20 @@ module.exports = {
 
 
   "Mongo": {
-    "ip": "104.236.231.11",
+    "ip": "",
     "port": "27017",
-    "dbname": "dvpdb",
-    "password": "DuoS123",
-    "user": "duo"
+    "dbname": "",
+    "password": "",
+    "user": ""
   },
 
   "Services": {
-    "accessToken": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdWtpdGhhIiwianRpIjoiMTdmZTE4M2QtM2QyNC00NjQwLTg1NTgtNWFkNGQ5YzVlMzE1Iiwic3ViIjoiNTZhOWU3NTlmYjA3MTkwN2EwMDAwMDAxMjVkOWU4MGI1YzdjNGY5ODQ2NmY5MjExNzk2ZWJmNDMiLCJleHAiOjE4OTMzMDI3NTMsInRlbmFudCI6LTEsImNvbXBhbnkiOi0xLCJzY29wZSI6W3sicmVzb3VyY2UiOiJhbGwiLCJhY3Rpb25zIjoiYWxsIn1dLCJpYXQiOjE0NjEyOTkxNTN9.YiocvxO_cVDzH5r67-ulcDdBkjjJJDir2AeSe3jGYeA",
-    "callcontrolHost": "resourceservice.app.veery.cloud",
+    "accessToken": "",
+    "callcontrolHost": "",
     "callcontrolPort": "8831",
     "callcontrolVersion": "1.0.0.0",
 
-    "fileserviceurl": 'fileservice.app.veery.cloud',
+    "fileserviceurl": '',
     "fileserviceport": '8081',
     "fileserviceVersion": "1.0.0.0"
   },
